770c8da4e943f1ff0586a3034f6d41541d35ef10
Anuj Gupta <anuj@flick2know.com>
2023-09-19 15:49:13 +0530

    removed waitress

--- app.py
+++ app.py
@@ -70,11 +70,11 @@ def predict():
 
 if __name__ == "__main__":
     #for production
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=8080)
 
     #debug
-    #app.run() 
+    app.run() 
 
 
  
